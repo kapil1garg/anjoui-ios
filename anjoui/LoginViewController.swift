952a6397934612eb64236041971bfc6ae890f04b
Kapil Arun Garg 3 <kgarg6@sapient.com>
2015-10-08 03:09:58 -0500

    aws-based login system

--- anjoui/LoginViewController.swift
+++ anjoui/LoginViewController.swift
@@ -17,64 +17,86 @@ class LoginViewController: UIViewController {
     @IBAction func loginClick(sender: UIButton) {
         // Authenticate user
         let user_email:String = (txtEmail.text! as String).stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
-        let hashed_user_pass:String = ("Add some salt" + (txtPassword.text! as String) + "maybe some pepper").MD5()
+        let password:String = txtPassword.text! as String
         
-        if (user_email == "" || hashed_user_pass == "") {
-            var alertView:UIAlertView = UIAlertView()
+        // check for valid input
+        if (user_email == "" || password == "") {
+            let alertView:UIAlertView = UIAlertView()
             alertView.title = "Please enter Email and Password"
             alertView.message = "Email and Password fields cannot be blank."
             alertView.delegate = self
             alertView.addButtonWithTitle("OK")
             alertView.show()
         } else if (!user_email.isValidEmail()){
-            var alertView:UIAlertView = UIAlertView()
+            let alertView:UIAlertView = UIAlertView()
             alertView.title = "Invalid email"
             alertView.message = "Email must be in the format: example@anjoui.com"
             alertView.delegate = self
             alertView.addButtonWithTitle("OK")
             alertView.show()
         } else {
-            let usertable = self.client!.tableWithName("users")
+            // create post data string
+            let post:NSString = "email=\(user_email)&password=\(password)"
+            NSLog("PostData: %@",post);
             
-            let userCheckPredicate = NSPredicate(format: "email == %@", user_email)
-            usertable!.readWithPredicate(userCheckPredicate, completion: {
-                (results, error) in
-                if (error != nil)  {
-                    NSLog(String(format: "%@", error.debugDescription))
-                }
+            // create connection URL
+            let url: NSURL = NSURL(string: "http://ec2-52-23-188-123.compute-1.amazonaws.com/anjoui/login.php")!
+            let postData:NSData = post.dataUsingEncoding(NSASCIIStringEncoding)!
+            let postLength:NSString = String( postData.length )
+            
+            let request:NSMutableURLRequest = NSMutableURLRequest(URL: url)
+            request.HTTPMethod = "POST"
+            request.HTTPBody = postData
+            request.setValue(postLength as String, forHTTPHeaderField: "Content-Length")
+            request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+            request.setValue("application/json", forHTTPHeaderField: "Accept")
+            
+            var response: NSURLResponse?
+            do {
+                let urlData: NSData? = try NSURLConnection.sendSynchronousRequest(request, returningResponse: &response)
+                if ( urlData != nil ) {
+                    let res = response as! NSHTTPURLResponse!;
+                    NSLog("Response code: %ld", res.statusCode);
                 
-                if (results.items.count != 0) {
-                    let valid_pass = results.items[0]["password"] as! String
-                    
-                    if (hashed_user_pass == valid_pass) {
-                        print("User is valid")
-                        self.performSegueWithIdentifier("login_user", sender: nil)
-                    } else {
-                        var alertView:UIAlertView = UIAlertView()
+                    if (res.statusCode == 200) {
+                        do {
+                            let jsonData:NSDictionary = try NSJSONSerialization.JSONObjectWithData(urlData!, options: NSJSONReadingOptions.MutableContainers) as! NSDictionary
+                            self.performSegueWithIdentifier("login_user", sender: nil)
+                        } catch let error as NSError {
+                            print(error)
+                        }
+                    } else if (res.statusCode == 403) {
+                        let responseData:NSString  = NSString(data:urlData!, encoding:NSUTF8StringEncoding)!
+                        
+                        let alertView:UIAlertView = UIAlertView()
                         alertView.title = "Sign in Failed"
-                        alertView.message = "Incorrect Password"
                         alertView.delegate = self
                         alertView.addButtonWithTitle("OK")
+
+                        if (responseData == "Password is incorrect." || responseData == "Email does not exist.") {
+                            alertView.message = responseData as String
+                        } else {
+                            alertView.message = "Connection Failed"
+
+                        }
                         alertView.show()
                     }
                 } else {
-                    var alertView:UIAlertView = UIAlertView()
+                    let alertView:UIAlertView = UIAlertView()
                     alertView.title = "Sign in Failed"
-                    alertView.message = "Email does not exist"
+                    alertView.message = "Connection Failure"
                     alertView.delegate = self
                     alertView.addButtonWithTitle("OK")
                     alertView.show()
                 }
-            })
+            } catch let error as NSError {
+                print(error)
+            }
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.client = MSClient(
-            applicationURLString:"https://anjoui-test-ale.azure-mobile.net/",
-            applicationKey:"EVLaGLPcRrWygXxmjjpYFviRlsfUgE86"
-        )
         // Do any additional setup after loading the view.
         
         //Looks for single or multiple taps.
